30df46cb9cf8042540a316ad529e6bc7f91099dc
Akansha <rinkujoon4@gmail.com>
2023-06-11 14:35:20 +0530

    material-ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
- 
- 
+import './app.scss'
+import Home from "./home/Home";
 
 function App() {
   return (
     <div className="App">
-      jnht
+     <Home/>
     </div>
   );
 }
---
+++ src/home/Home.jsx
@@ -0,0 +1,13 @@
+import "./home.scss"
+import AccessibleIcon from '@mui/icons-material/Accessible';
+ const Home = () => {
+   return (
+     <div className='home' >
+         <AccessibleIcon/>
+         
+        </div>
+   )
+ }
+ 
+ export default Home
+ 
