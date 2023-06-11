3e8b240f7136d334d795123a53edcc8e33702095
Akansha <rinkujoon4@gmail.com>
2023-06-12 00:06:20 +0530

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './app.scss'
+ 
+ 
 import Home from "./home/Home";
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+     <Home/> 
+ 
     </div>
   );
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,12 @@
 import "./home.scss"
-import AccessibleIcon from '@mui/icons-material/Accessible';
+import Navbar from "../components/navbar/Navbar"
+ 
  const Home = () => {
    return (
      <div className='home' >
-         <AccessibleIcon/>
-         
+   
+       <Navbar/>
+       <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"  width="100%"alt=""   />
         </div>
    )
  }
